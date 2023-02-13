a9f4759e70b5404d70cfaa295792340ac779449c
TrinhTuan-2002-fpt <tuantu2k2badaoyl@gmail.com>
2023-02-13 14:14:55 +0700

    init folder and axios call api

---
+++ src/utils/http/AuthRequest.ts
@@ -0,0 +1,41 @@
+import axios, {
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+const AuthService = axios.create({
+  baseURL: `https://localhost:7236/api`,
+  timeout: 5000,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "X-Requested-With",
+  },
+});
+
+AuthService.interceptors.request.use(
+  (configs: InternalAxiosRequestConfig) => {
+    // console.log(JSON.stringify(configs));
+    console.log(configs.headers);
+    // configs.headers["Authorization"] = `Bearer ${getToken()}`;
+    configs.headers["Content-Type"] = "application/json";
+    return configs;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+AuthService.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  async (error: AxiosError) => {
+    if (error?.response) {
+      return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error.message);
+  }
+);
+
+export default AuthService;
